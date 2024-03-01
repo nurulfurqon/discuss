2d8f8e36441ac13b6eba0bb8f6f979f55a4b1632
Nurul Furqon <furqon.mascode@gmail.com>
2024-03-01 14:22:32 +0700

    fix: update vscode settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full flex items-center justify-center">
       <Button>Click Me!!</Button>
     </div>
   );
