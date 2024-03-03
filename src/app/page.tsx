927612f17dad8f8384493c7f64f0cdfaa1625186
Nurul Furqon <furqon.mascode@gmail.com>
2024-03-03 16:51:32 +0700

    feat: add top post on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Divider } from "@nextui-org/react";
 import TopicCreateForm from "@/components/topics/topic-create-form";
 import TopicList from "@/components/topics/topic-list";
+import PostList from "@/components/posts/post-list";
+import { fetchTopPosts } from "@/db/queries/posts";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h2 className="text-xl font-semibold text-gray-800 mb-3">Top Posts</h2>
+        <PostList fetchData={fetchTopPosts} />
       </div>
       <div className="space-y-4">
         <div className="flex items-end justify-between">
